28552c0fa0765cfda70fbb75fc61acb078381da1
M Farkas-Dyck <strake888@gmail.com>
2016-03-28 21:28:43 -0800

    impl glium::vertex::Attribute

--- src/linea_glium.rs
+++ src/linea_glium.rs
@@ -1,5 +1,6 @@
 use core::mem;
 use glium::uniforms::*;
+use glium::vertex::*;
 use typenum::consts::*;
 
 use super::*;
@@ -23,3 +24,19 @@ impl_AsUniformValue!(Matrix<f32, U4, U4>, [[f32; 4]; 4]);
 impl_AsUniformValue!(Matrix<f64, U2, U2>, [[f64; 2]; 2]);
 impl_AsUniformValue!(Matrix<f64, U3, U3>, [[f64; 3]; 3]);
 impl_AsUniformValue!(Matrix<f64, U4, U4>, [[f64; 4]; 4]);
+
+macro_rules! impl_Attribute {
+    ($T: ty, $M: ty, $N: ty, $AT: ident) =>
+        (unsafe impl Attribute for Matrix<$T, $M, $N> {
+             #[inline] fn get_type() -> AttributeType { AttributeType::$AT }
+         })
+}
+
+impl_Attribute!(f32, U1, U1, F32);
+impl_Attribute!(f32, U2, U1, F32F32);
+impl_Attribute!(f32, U3, U1, F32F32F32);
+impl_Attribute!(f32, U4, U1, F32F32F32F32);
+impl_Attribute!(f64, U1, U1, F64);
+impl_Attribute!(f64, U2, U1, F64F64);
+impl_Attribute!(f64, U3, U1, F64F64F64);
+impl_Attribute!(f64, U4, U1, F64F64F64F64);
